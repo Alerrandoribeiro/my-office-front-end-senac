17497fb3e7decf088c65bec6776d43180ae67693
Glauberty Ribeiro <glaubertyribeiro007@gmail.com>
2026-05-09 14:32:22 -0300

    Finalizando componente atómo Avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
-import './App.css'
 import Botao from './components/atomos/Botao/Botao'
-import Icone from './components/atomos/Icone/Icone'
 import { FaSearch } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import InputDate from './components/atomos/InputDate/InputDate';
 import InputTexto from './components/atomos/InputTexto/InputTexto';
+import Avatar from './components/atomos/Avatar/Avatar';
 
 
 
@@ -14,6 +12,7 @@ function App() {
     <>
       <Botao cor={"erro"} texto={"Excluir"} largura="150px" altura="50px" icone={<MdDelete />} />
       <Botao cor={"secundaria"} texto={"Buscar"} largura="150px" altura="50px" icone={<FaSearch/>} />
+      <Avatar imagem={"/favicon.svg"} nome="John Doe" />
       <InputTexto tipo='number' largura='300px' cor='primaria' placeholder='Digite seu nome' />
       <InputDate desabilitado = {true} largura='300px' cor='primaria' />
     </>
--- src/components/atomos/Avatar/Avatar.jsx
+++ src/components/atomos/Avatar/Avatar.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './Avatar.css'
+import React from "react";
+import "./Avatar.css";
 
-const Avatar = () => {
+const Avatar = ({
+  imagem,
+  nome = "Usuário",
+  tamanho = "60px",
+  borda = false,
+  aoClicar,
+}) => {
   return (
-    <div>Avatar</div>
-  )
-}
+    <div
+      className={`avatar_root ${borda ? "avatar_borda" : ""}`}
+      onClick={aoClicar}
+      style={{
+        width: tamanho,
+        height: tamanho,
+      }}
+    >
+      {imagem ? (
+        <img
+          src={imagem}
+          alt={nome}
+          className="avatar_imagem"
+        />
+      ) : (
+        <span className="avatar_texto">
+          {nome.charAt(0).toUpperCase()}
+        </span>
+      )}
+    </div>
+  );
+};
 
-export default Avatar
+export default Avatar;
