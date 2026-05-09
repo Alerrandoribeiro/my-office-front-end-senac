f1c60d1a549a9c03a4fed641a5dd64b4366a215a
Glauberty Ribeiro <glaubertyribeiro007@gmail.com>
2026-05-09 14:08:26 -0300

    subindo input de calendario

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Botao from './components/atomos/Botao/Botao'
 import Icone from './components/atomos/Icone/Icone'
 import { FaSearch } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import InputDate from './components/atomos/InputDate/InputDate';
 
 
 
@@ -12,6 +13,7 @@ function App() {
     <>
       <Botao cor={"erro"} texto={"Excluir"} largura="150px" altura="50px" icone={<MdDelete />} />
       <Botao cor={"secundaria"} texto={"Buscar"} largura="150px" altura="50px" icone={<FaSearch/>} />
+      <InputDate desabilitado = {false} largura='300px' cor='primaria' />
     </>
   )
 }
--- src/components/atomos/InputDate/InputDate.jsx
+++ src/components/atomos/InputDate/InputDate.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import './InputDate.css'
+import React, { useRef } from "react";
+import "./InputDate.css";
 
-const InputDate = () => {
+const InputDate = ({
+  valor,
+  cor = "padrao",
+  aoAlterar,
+  largura = "200px",
+  altura = "50px",
+  desabilitado = false,
+}) => {
+  const inputRef = useRef(null);
+
+  const estilos = ["input-data_root"];
+
+  switch (cor) {
+    case "primaria":
+      estilos.push("input-data_primario");
+      break;
+
+    case "erro":
+      estilos.push("input-data_erro");
+      break;
+
+    default:
+      estilos.push("input-data_padrao");
+      break;
+  }
+
+  const abrirCalendario = () => {
+    inputRef.current?.showPicker();
+  };
+
   return (
-    <div>InputDate</div>
-  )
-}
+    <input
+      ref={inputRef}
+      className={estilos.join(" ")}
+      type="date"
+      value={valor}
+      onChange={aoAlterar}
+      onClick={abrirCalendario}
+      disabled={desabilitado}
+      style={{
+        width: largura,
+        height: altura,
+      }}
+    />
+  );
+};
 
-export default InputDate
+export default InputDate;
